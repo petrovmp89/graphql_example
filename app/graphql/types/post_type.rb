21857e962218304909b5d136d1198650bd4c0d2a
Maksim Petrov <petrovmp89@gmail.com>
2018-04-18 23:32:00 +0300

    Add posts and comments

---
+++ app/graphql/types/post_type.rb
@@ -0,0 +1,7 @@
+Types::PostType = GraphQL::ObjectType.define do
+  name "Post"
+  field :id, types.Int
+  field :title, types.String
+  field :rating, types.Int
+  field :comments, types[Types::CommentType]
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,10 @@ Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
-
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
+  field :post do
+      type Types::PostType
+      argument :id, !types.ID
+      description "Find a Post by ID"
+      resolve ->(obj, args, ctx) { Post.find(args["id"]) }
   end
 end
